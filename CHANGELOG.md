b2e0e04a39ce6820ccc343c22ac687fcd7291a37
Rougin Gutib <rougingutib@gmail.com>
2018-09-28 11:47:45 +0800

    Update scraper and test cases

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+All notable changes to `ABS-CBN News` will be documented in this file.
+
+## 0.1.0 - Unreleased
+
+### Added
+- `ABS-CBN News` library
--- src/Scraper.php
+++ src/Scraper.php
@@ -3,9 +3,9 @@
 namespace Pilipinews\Website\Abscbn;
 
 use Pilipinews\Common\Article;
+use Pilipinews\Common\Crawler as DomCrawler;
 use Pilipinews\Common\Interfaces\ScraperInterface;
 use Pilipinews\Common\Scraper as AbstractScraper;
-use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * ABS-CBN News Scraper
@@ -23,20 +23,31 @@ class Scraper extends AbstractScraper implements ScraperInterface
     /**
      * @var string[]
      */
-    protected $texts = array('I-refresh ang pahinang ito para sa updates.', 'Refresh this link for more details.');
+    protected $texts = array(
+        'I-refresh ang pahinang ito para sa updates.',
+        'Refresh this link for more details.',
+        'I-refresh ang web page na ito para sa mga pinakahuling update.'
+    );
 
     /**
      * Returns the contents of an article.
      *
+     * @param  string $link
      * @return \Pilipinews\Common\Article
      */
-    public function scrape()
+    public function scrape($link)
     {
+        $this->prepare(mb_strtolower($link));
+
         $title = $this->title('h1.news-title');
 
         $this->remove((array) $this->removables);
 
         $body = $this->body('.article-content');
+
+        $body = $this->album($body);
+
+        $body = $this->embedly($body);
 
         $body = $this->image($body);
 
@@ -44,20 +55,82 @@ class Scraper extends AbstractScraper implements ScraperInterface
 
         $body = $this->video($body);
 
+        $body = $this->post($body);
+
         $html = $this->html($body, $this->texts);
 
-        return new Article($title, (string) $html);
+        $article = new Article($title, (string) $html);
+
+        file_put_contents('demo6.html', $article->post());
+
+        return $article;
     }
 
     /**
-     * Converts image elements to readable string.
+     * Converts an album element into a readable string.
      *
      * @param  \Symfony\Component\DomCrawler\Crawler $crawler
      * @return \Symfony\Component\DomCrawler\Crawler
      */
-    protected function image(Crawler $crawler)
+    protected function album(DomCrawler $crawler)
     {
-        $callback = function (Crawler $crawler, $html) {
+        $callback = function (DomCrawler $crawler, $html) {
+            $results = array();
+
+            $pattern = '.slider-for > div > img';
+
+            $items = $crawler->filter($pattern);
+
+            $pattern = '.slider-desc > .item-desc > p';
+
+            $texts = $crawler->filter($pattern);
+
+            for ($i = 0; $i < $items->count(); $i++) {
+                $link = 'PHOTO: ' . $items->eq($i)->attr('src');
+
+                $text = '';
+
+                if ($texts->count() !== 1) {
+                    $text = $texts->eq($i)->text();
+                }
+
+                $result = '<p>' . $link . ' - ' . $text . '</p>';
+
+                $results[] = str_replace(' - </', '</', $result);
+            }
+
+            return implode("\n\n", (array) $results);
+        };
+
+        return $this->replace($crawler, '.media-content', $callback);
+    }
+
+    /**
+     * Converts an embedly elements into a readable string.
+     *
+     * @param  \Symfony\Component\DomCrawler\Crawler $crawler
+     * @return \Symfony\Component\DomCrawler\Crawler
+     */
+    protected function embedly(DomCrawler $crawler)
+    {
+        $callback = function (DomCrawler $crawler) {
+            $item = $crawler->filter('a')->first();
+
+            return 'EMBED: ' . $item->attr('href');
+        };
+
+        return $this->replace($crawler, '.embedly-card', $callback);
+    }
+
+    /**
+     * Converts image elements into a readable string.
+     *
+     * @param  \Symfony\Component\DomCrawler\Crawler $crawler
+     * @return \Symfony\Component\DomCrawler\Crawler
+     */
+    protected function image(DomCrawler $crawler)
+    {
+        $callback = function (DomCrawler $crawler, $html) {
             $image = 'PHOTO: ' . $crawler->filter('img')->attr('src');
 
             $image = str_replace('?ext=.jpg', '', (string) $image);
@@ -77,14 +150,29 @@ class Scraper extends AbstractScraper implements ScraperInterface
     }
 
     /**
-     * Converts video elements to readable string.
+     * Converts post elements into a readable string.
      *
      * @param  \Symfony\Component\DomCrawler\Crawler $crawler
      * @return \Symfony\Component\DomCrawler\Crawler
      */
-    protected function video(Crawler $crawler)
+    protected function post(DomCrawler $crawler)
     {
-        $callback = function (Crawler $crawler) {
+        $callback = function (DomCrawler $node, $html) {
+            return '<p>POST: ' . $node->attr('data-href') . '</p>';
+        };
+
+        return $this->replace($crawler, '.fb-post', $callback);
+    }
+
+    /**
+     * Converts video elements into a readable string.
+     *
+     * @param  \Symfony\Component\DomCrawler\Crawler $crawler
+     * @return \Symfony\Component\DomCrawler\Crawler
+     */
+    protected function video(DomCrawler $crawler)
+    {
+        $callback = function (DomCrawler $crawler) {
             $element = $crawler->filter('iframe');
 
             $link = $element->attr('src');
